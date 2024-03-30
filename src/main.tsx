821e602db1ce7e3d194641dcc440197769a14e6a
Timo Huynh <t.huynh@sprinteins.com>
2024-03-30 12:48:46 +0100

    fix broken setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./main.css";
+import "./main.scss";
 import App from "./App";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
